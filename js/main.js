4e2c6e3ba223efadf663816e0adfcb29345e1051
Wilder Ledes <wilder@ledes.me>
2019-02-26 16:32:03 -0500

    Added: main.js and navbar toggling

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const menuButton = document.querySelector('.nav-btn');
+const nav = document.querySelector('.navbar');
+
+// Listener function for menu button
+function toggleButton(evt) {
+    nav.classList.toggle('open');
+}
+
+// Menu event listener
+menuButton.addEventListener('click', toggleButton);
